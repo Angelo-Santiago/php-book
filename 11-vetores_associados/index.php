83a9a4e17461b71adef0a02c64b7739088d1623c
Ângelo <angelosantiago410@gmail.com>
2023-09-19 17:05:50 -0300

    atualizando

---
+++ 11-vetores_associados/index.php
@@ -0,0 +1,9 @@
+<?php 
+    $v = array ("Nome" => array("Angelo", "Carlos", "eduardo"),
+    "Idade" => array(0.6,24,7), 
+    "Altura"=> array(0.7, 1.78, 1.24));
+
+    echo "Nome : ".$v["Nome"][0]."<br>Idade :".$v["Idade"][0]." <br> Altura  : " .$v["Altura"][0];
+    echo "<br>Nome : ".$v["Nome"][1]."<br>Idade :".$v["Idade"][1]." <br> Altura  : " .$v["Altura"][1];
+    echo "<br>Nome : ".$v["Nome"][2]."<br>Idade :".$v["Idade"][2]." <br> Altura  : " .$v["Altura"][2];
+?>
