dfacd9e935cc4c891b9b72132af82299a810e510
Ângelo <angelosantiago410@gmail.com>
2023-09-23 20:42:28 -0300

    Create index.php

---
+++ 12-estrutura_decisao/index.php
@@ -0,0 +1,12 @@
+<?php 
+    $nota = 9;
+    $nota2 = 5;
+    $media = ($nota + $nota2 )/2;
+    echo "media = ".$media;
+    if($media >= 7.0) {
+        echo "<br> Aluno Aprovado";
+    }
+    else{
+        echo "<br> Aluno Reprovado";
+    }
+?>
